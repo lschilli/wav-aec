8476ba2df0f82818cd720a539a84041bf88295a9
Lars Schillingmann <lars@schillingmann.net>
2016-04-03 16:34:29 +0200

    added no dither

--- run-wav-aec.sh
+++ run-wav-aec.sh
@@ -7,4 +7,4 @@ fi
 
 builddir="${BASH_SOURCE[0]%/*}/build"
 
-"$builddir/wav-aec" <(exec sox -R "$1" -r 16000 -t raw -e signed-integer -b 16 - remix "$2") <(exec sox -R "$3" -r 16000 -t raw -e signed-integer -b 16 - remix "$4") >(exec sox -r 16000 -t raw -e signed-integer -b 16 - "$5")
+"$builddir/wav-aec" <(exec sox -DR "$1" -r 16000 -t raw -e signed-integer -b 16 - remix "$2") <(exec sox -DR "$3" -r 16000 -t raw -e signed-integer -b 16 - remix "$4") >(exec sox -DR -r 16000 -t raw -e signed-integer -b 16 - "$5")
