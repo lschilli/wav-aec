8458117b8a59ede14d6620578dbda291a2d5e703
Lars Schillingmann <lars@schillingmann.net>
2016-04-06 01:53:48 +0200

    restruct

--- run-wav-aec.sh
+++ run-wav-aec.sh
@@ -5,6 +5,7 @@ if [ "$#" -ne 5 ]; then
 	exit 1
 fi
 
+samplerate=48000
 builddir="${BASH_SOURCE[0]%/*}/build"
 
-"$builddir/wav-aec" <(exec sox -DR "$1" -r 16000 -t raw -e signed-integer -b 16 - remix "$2") <(exec sox -DR "$3" -r 16000 -t raw -e signed-integer -b 16 - remix "$4") >(exec sox -DR -r 16000 -t raw -e signed-integer -b 16 - "$5")
+"$builddir/webrtc-audioproc" -filter_aec -aec_level 2 -filter_ns -ns_level 1 -near_in_sr 48000 -near_in <(exec sox -DR "$1" -r $samplerate -t raw -e signed-integer -b 16 - remix "$2") -far_in <(exec sox -DR "$3" -r $samplerate -t raw -e signed-integer -b 16 - remix "$4") -near_out >(exec sox -DR -r $samplerate -t raw -e signed-integer -b 16 - "$5")
